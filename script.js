88793dffb81a9dbd58d58a38d540d11e7b4c8513
rahul4328 <125386949+rahul4328@users.noreply.github.com>
2023-05-18 08:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-window.promises = [];
+const promises = [
+      new Promise((resolve) => setTimeout(() => resolve("Promise 1 resolved"), getRandomTime())),
+      new Promise((resolve) => setTimeout(() => resolve("Promise 2 resolved"), getRandomTime())),
+      new Promise((resolve) => setTimeout(() => resolve("Promise 3 resolved"), getRandomTime())),
+      new Promise((resolve) => setTimeout(() => resolve("Promise 4 resolved"), getRandomTime())),
+      new Promise((resolve) => setTimeout(() => resolve("Promise 5 resolved"), getRandomTime()))
+    ];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+    // Use Promise.any to wait for the first promise to resolve
+    Promise.any(promises)
+      .then((result) => {
+        // Print the result to the output div
+        const outputDiv = document.getElementById("output");
+        outputDiv.textContent = result;
+      });
+
+    // Helper function to generate a random time between 1 and 5 seconds
+    function getRandomTime() {
+      return Math.floor(Math.random() * 5000) + 1000;
+    }`
